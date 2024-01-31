faf7ead35fb7b80ecaf7df37749d8e315becc4a1
Michael Semle <git@mikeproduction.de>
2024-01-31 20:46:06 +0100

    [TASK] Add ext icon

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'trueprogramming_extension' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:instagram/Resources/Public/Icons/ext_icon.svg',
+    ],
     'instagram-module' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
         'source' => 'EXT:instagram/Resources/Public/Icons/module.png',
